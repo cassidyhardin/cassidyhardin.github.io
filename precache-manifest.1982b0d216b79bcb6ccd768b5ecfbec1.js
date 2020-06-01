d9f7bb40726995c4e76bc3c4613758b3f7299615
cassidyhardin <cassidyhardin@berkeley.edu>
2020-06-01 22:30:10 +0900

    Updates

---
+++ precache-manifest.1982b0d216b79bcb6ccd768b5ecfbec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f803753ffe2d38911c7fc378fa3e11d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca5aaec419902a25629",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ab5e830fce39b5b5aa3",
+    "url": "/static/js/2.6ee34059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ee34059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca5aaec419902a25629",
+    "url": "/static/js/main.e082ee45.chunk.js"
+  },
+  {
+    "revision": "1c8f035327f40c5a37b9",
+    "url": "/static/js/runtime-main.0864920b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

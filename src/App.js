f0a5796023a1501bd6f77ea0696c69e42e16fb1d
cassidyhardin <cassidyhardin@berkeley.edu>
2020-06-12 15:50:47 -0700

    component strucutre

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
+import cassidy from './cassidy.jpeg'
 import './App.css';
+import About from './components/about'
+import Navbar from './components/navbar'
+import Projects from './components/projects'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={cassidy} className='display-picture' alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+         Cassidy Hardin
+
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/cassidyhardin/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin
         </a>
+
+        <a
+          className="App-link"
+          href="https://github.com/cassidyhardin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+
+
+
+        
       </header>
+
+      <About></About>
+      <Projects></Projects> 
+      <Navbar> </Navbar> 
     </div>
   );
 }
---
+++ src/components/projects.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export default class Projects extends Component {
+  render() {
+    return (
+        <div className='project-card'>
+            <div className='project-name'>
+                TarjimlyDocs
+            </div>
+
+            <div className='project-content'>
+                Built a ruby on rails web-app connecting Aid Workers and Refugees with Volunteer Translators. 
+                Aid Workers are able to submit documents, which are claimed and completed by Translators
+            </div>
+
+
+            <a
+          className="project-github"
+          href="https://github.com/cassidyhardin/TarjimlyDocs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+
+        <a
+          className="project-github"
+          href="https://docs.tarjimly.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TarjimlyDocs
+        </a>
+
+        <div className='project-features'>
+            <li>
+                Ruby on Rails 
+                AWS
+                Docker
+            </li>
+        </div>
+
+
+    </div>
+
+        
+    )
+  }
+}
